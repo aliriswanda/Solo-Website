3c6cc7d86e6d18351ee1eee7d0644aeda4b2dc09
aliriswanda <imawaniwanali@gmail.com>
2019-11-28 16:08:14 +0700

    Menambah section Talent

--- js/script.js
+++ js/script.js
@@ -1,5 +1,53 @@
+/* Preloader */
+
 $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
 
+
+/* Team */
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            }
+        }
+    });
+});
+
+/* Progress Bars */
+
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+});
